9e1c246a0d3f0ca52758074e5f758da23a42e168
rayenbenzid <115357029+rayenbenzid@users.noreply.github.com>
2023-08-09 10:21:09 +0100

    main

---
+++ style.css
@@ -0,0 +1,22 @@
+body {
+  font-family: 'Roboto', sans-serif;
+}
+
+.navbar {
+  display: inline;
+}
+
+.creative-heading {
+  color: #ff9900;
+  font-size: 24px;
+  text-decoration: underline;
+}
+
+.creative-button {
+  background-color: #3399ff;
+  color: #fff;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
